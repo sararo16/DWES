a47ed66b69d27f8744310694ee28fbda99d62def
sararo16 <sararo16@educastur.es>
2025-12-04 17:43:36 +0100

    a

--- UNIDAD 4/Validaciones/PractiaExamen2024/resultado.php
+++ UNIDAD 4/Validaciones/PractiaExamen2024/resultado.php
@@ -10,13 +10,23 @@ if (!isset($_SESSION['login'])) {
     header("Location: index.php");
     exit();
 }
+$hoy=date ("Y-m-d");
+
+$consultaSolucion=$connection-> query ("SELECT solucion FROM solucion WHERE fecha='$hoy'");
+if ($consultaSolucion->num_rows==0){
+    echo "<p>No hay solución registrada para hoy ($hoy). </p>";
+    exit ();
+}
+$solucion=trim($consultaSolucion->fetch_assoc()['solucion']);
+
+
 
 
 
 echo <<<_END
 <html>
     <body>
-        <h1>Fecha: <time datetime="2025-12-04">2025-12-04</time></h1>
+        <h1>Fecha: $hoy</h1>
 
 </body>
 </html>
