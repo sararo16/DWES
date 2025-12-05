9943736aff4dc9c82835ca745c50d8f836c56d00
Sara Rodríguez <sararo16@educastur.es>
2025-12-05 10:41:57 +0100

    avances

--- UNIDAD 4/Validaciones/PractiaExamen2024/resultado.php
+++ UNIDAD 4/Validaciones/PractiaExamen2024/resultado.php
@@ -17,7 +17,20 @@ if ($consultaSolucion->num_rows==0){
     echo "<p>No hay solución registrada para hoy ($hoy). </p>";
     exit ();
 }
-$solucion=trim($consultaSolucion->fetch_assoc()['solucion']);
+
+//sumar puntos
+$puntoDia =$connection->query ("UPDATE jugador j INNER JOIN respuestas r ON j.login INNER JOIN solucion s ON r.fecha = s.fecha 
+SET j.puntos=j.puntos+1
+WHERE r.respuesta='$hoy'");
+
+//resultados
+$aciertos= $connection->query ("SELECT login, hora FROM respuestas r
+INNER JOIN solucion s ON r.fecha=s.fecha
+WHERE r.respuesta=s.solucion AND r.fecha='$hoy'");
+
+$fallos= $connection->query ("SELECT login, hora FROM respuestas r
+INNER JOIN solucion s ON r.fecha=s.fecha
+WHERE r.respuesta!=s.solucion AND r.fecha='$hoy'");
 
 
 
@@ -27,7 +40,27 @@ echo <<<_END
 <html>
     <body>
         <h1>Fecha: $hoy</h1>
-
+        <h2>Jugadores acertantes: $aciertos->num_rows</h2>
+        <table>
+            <tr>
+                <th>Login</th>
+                <th>Hora</th>
+            </tr>
+            $aciertos->data_seek(0);
+            while ($aciertos->fetch_assoc()) {
+                echo "<tr><td>".$aciertos->login."</td><td>".$aciertos->hora."</td></tr>";
+            }   
+        </table>
+        <h2>Jugadores que han fallado: $fallos->num_rows</h2>
+        <table>
+            <tr>
+                <th>Login</th>
+                <th>Hora</th>
+            </tr>
+            $fallos->data_seek(0);
+            while ($fallos->fetch_assoc()) {
+                echo "<tr><td>".$fallos->login."</td><td>".$fallos->hora."</td></tr>";
+            }
 </body>
 </html>
 _END;
