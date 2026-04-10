2d393f176601c8c28051417d533b99a36f71623f
sararo16 <sararo16@educastur.es>
2026-04-10 20:25:33 +0200

    first commit

---
+++ LARAVEL/MIVIDEOCLUB/resources/views/catalog/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+     @section('content') Añadir pelicula 
+     
+
+@stop 
---
+++ LARAVEL/MIVIDEOCLUB/resources/views/catalog/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+     @section('content') Modificar película {{$id}}
+     
+
+@stop 
