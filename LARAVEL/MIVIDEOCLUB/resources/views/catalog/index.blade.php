198fb6053856c47871adc93fe8e315aac8a5a3a8
Sara Rodríguez <sararo16@educastur.es>
2026-04-10 12:35:28 +0200

    sdsd

---
+++ LARAVEL/MIVIDEOCLUB/resources/views/catalog/index.blade.php
@@ -0,0 +1,6 @@
+
+@extends('layouts.master')
+
+@section('content')Listado películas
+    
+@stop
--- LARAVEL/MIVIDEOCLUB/routes/web.php
+++ LARAVEL/MIVIDEOCLUB/routes/web.php
@@ -3,5 +3,40 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+
 });
 
+Route::get('login', function()
+{
+return view('auth.login');
+});
+
+Route::get('logout', function()
+{
+return 'logout de usuario';
+});
+
+
+Route::get('catalog', function()
+{
+return view('catalog.index');
+});
+
+Route::get('catalog/show/{id}', function($id)
+{
+return view('catalog.show', array('id'=>$id));
+})
+->where('id', '[0-9]+');
+
+
+Route::get('catalog/create', function()
+{
+return view('catalog.create');
+});
+
+Route::get('catalog/edit/{id}', function($id)
+{
+return view('catalog.edit', array('id'=>$id));
+})
+->where('id', '[0-9]+');
